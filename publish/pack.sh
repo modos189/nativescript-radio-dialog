5bbffa2f6435b819bfa4899643165f978308fab7
Alexander Danilov <git@modos189.ru>
2025-06-26 00:55:40 +0500

    fix publish process by consolidating dependencies in src/package.json

--- publish/pack.sh
+++ publish/pack.sh
@@ -6,24 +6,20 @@ PACK_DIR=package;
 ROOT_DIR=..;
 PUBLISH=--publish
 
-install(){
-    npm i
-}
-
 pack() {
 
     echo 'Clearing /src and /package...'
-    node_modules/.bin/rimraf "$TO_SOURCE_DIR"
-    node_modules/.bin/rimraf "$PACK_DIR"
+    "$SOURCE_DIR"/node_modules/.bin/rimraf "$TO_SOURCE_DIR"
+    "$SOURCE_DIR"/node_modules/.bin/rimraf "$PACK_DIR"
 
     # copy src
     echo 'Copying src...'
-    node_modules/.bin/ncp "$SOURCE_DIR" "$TO_SOURCE_DIR"
+    "$SOURCE_DIR"/node_modules/.bin/ncp "$SOURCE_DIR" "$TO_SOURCE_DIR"
 
     # copy README & LICENSE to src
     echo 'Copying README and LICENSE to /src...'
-    node_modules/.bin/ncp "$ROOT_DIR"/LICENSE "$TO_SOURCE_DIR"/LICENSE
-    node_modules/.bin/ncp "$ROOT_DIR"/README.md "$TO_SOURCE_DIR"/README.md
+    "$SOURCE_DIR"/node_modules/.bin/ncp "$ROOT_DIR"/LICENSE "$TO_SOURCE_DIR"/LICENSE
+    "$SOURCE_DIR"/node_modules/.bin/ncp "$ROOT_DIR"/README.md "$TO_SOURCE_DIR"/README.md
 
     # compile package and copy files required by npm
     echo 'Building /src...'
@@ -41,7 +37,7 @@ pack() {
 
     # delete source directory used to create the package
     cd ..
-    node_modules/.bin/rimraf "$TO_SOURCE_DIR"
+    "$SOURCE_DIR"/node_modules/.bin/rimraf "$TO_SOURCE_DIR"
 }
 
-install && pack
+pack
